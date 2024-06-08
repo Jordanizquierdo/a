414395561199b998edad8b7c475340070fd83607
Jordanizquierdo <169169895+Jordanizquierdo@users.noreply.github.com>
2024-06-08 19:18:17 -0400

    Add files via upload

---
+++ assets/credenciales.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTVkeNdooLXj2BZVKFiIwwOW_HfByEa7M",
+    authDomain: "practica-4cb04.firebaseapp.com",
+    projectId: "practica-4cb04",
+    storageBucket: "practica-4cb04.appspot.com",
+    messagingSenderId: "67690103397",
+    appId: "1:67690103397:web:dfe5755c74ab180a667c8c",
+    measurementId: "G-FC82EZD5MC"}
